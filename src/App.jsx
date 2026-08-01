e3e3974200ff3689ae7c2f9a66bcec45256598e5
luubangtuvi2022-blip <luubangtuvi2022@gmail.com>
2026-08-01 15:31:31 +0700

    Configure gh-pages deployment and switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
@@ -6,21 +7,26 @@ import TodoList from './pages/TodoList';
 import KanbanBoard from './pages/KanbanBoard';
 import CalendarView from './pages/CalendarView';
 import History from './pages/History';
+import { TaskProvider } from './context/TaskContext';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="todo" element={<TodoList />} />
-          <Route path="kanban" element={<KanbanBoard />} />
-          <Route path="calendar" element={<CalendarView />} />
-          <Route path="history" element={<History />} />
-        </Route>
-      </Routes>
-    </Router>
+    <TaskProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="todo" element={<TodoList />} />
+            <Route path="kanban" element={<KanbanBoard />} />
+            <Route path="calendar" element={<CalendarView />} />
+            <Route path="history" element={<History />} />
+          </Route>
+        </Routes>
+      </Router>
+    </TaskProvider>
   );
 }
 
